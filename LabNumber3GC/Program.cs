22d457b85dcff8829bda42b0e7e9a5e3f0eafe44
Riyad <rayabed@gmail.com>
2018-04-25 11:00:42 -0400

    First Commit

---
+++ LabNumber3GC/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LabNumber3GC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UserInput;
+            bool UserContinue = true;
+
+            while (UserContinue == true)
+            {
+                Console.WriteLine("Please enter your first name");
+                string FirstName = (Console.ReadLine());
+                    Console.WriteLine("Please enter a whole number between 1 and 100");
+                UserInput = int.Parse(Console.ReadLine());
+
+                if (UserInput < 1 || UserInput > 100)
+                {
+                    Console.WriteLine("Remember " + FirstName + ", the number needs to be between 1 and 100");
+                }
+
+                else if (UserInput % 2 == 0 && UserInput <= 25)
+                {
+                    Console.WriteLine(FirstName + ", " + UserInput + " is even and less than 25");
+                }
+                else if (UserInput % 2 == 0)
+                {
+
+                    Console.WriteLine(FirstName + ", " + UserInput + " is even");
+                }
+                else
+                {
+                    Console.WriteLine(FirstName + ", " + UserInput + " is odd");
+                }
+
+                Console.WriteLine("Do you want to try again, " + FirstName + "? y/n");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "no" || choice.ToLower() == "n")
+                {
+                    UserContinue = false;
+                }
+            }
+
+
+
+        }
+
+    }
+}
